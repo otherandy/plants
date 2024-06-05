586ec7bc1ffc02453275aa471f59ed1e349c5899
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-06-04 20:24:13 -0700

    changed top-level await

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import { initDb } from "./lib/db.ts";
 
-try {
-  await initDb();
-} catch (err) {
+initDb().catch((err) => {
   if (err instanceof Error) console.error(err.message);
   else console.error("Something went wrong.");
-}
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
