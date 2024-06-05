71970051e4379e127ca01849f49c6e557cb6ff61
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-06-04 20:35:45 -0700

    md cols 2

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
         </Button>
       </nav>
       <main className="container pb-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {plants.map((plant: Plant) => (
             <PlantCard
               key={plant.id}
