f50a05c50e817d88505dbc7c14c4194539d44da5
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-10-01 18:56:20 -0700

    added image crop

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,13 +1,16 @@
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import Resizer from "react-image-file-resizer";
+import ImageCrop from "./ImageCrop";
 
 export default function FileUpload({
   updateData,
 }: {
   updateData: (data: string) => void;
 }) {
+  const [src, setSrc] = useState<string>();
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file) => {
       Resizer.imageFileResizer(
@@ -18,14 +21,14 @@ export default function FileUpload({
         80,
         0,
         (uri) => {
-          console.log(uri);
-          updateData(uri as string);
+          setSrc(uri as string);
         },
         "base64",
       );
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: { "image/*": [] },
@@ -33,13 +36,19 @@ export default function FileUpload({
   });
 
   return (
-    <AspectRatio
-      ratio={4 / 3}
-      className="flex items-center text-center border-accent-foreground border border-dashed rounded-md cursor-pointer p-10 hover:bg-accent transition-colors"
-      {...getRootProps()}
-    >
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
-    </AspectRatio>
+    <>
+      {src ? (
+        <ImageCrop src={src} updateData={updateData} />
+      ) : (
+        <AspectRatio
+          ratio={4 / 3}
+          className="flex items-center text-center border-accent-foreground border border-dashed rounded-md cursor-pointer p-10 hover:bg-accent transition-colors"
+          {...getRootProps()}
+        >
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </AspectRatio>
+      )}
+    </>
   );
 }
---
+++ src/components/ImageCrop.tsx
@@ -0,0 +1,97 @@
+import { useRef, useState } from "react";
+import ReactCrop, {
+  type Crop,
+  centerCrop,
+  makeAspectCrop,
+} from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import { Button } from "@/components/ui/button";
+
+function centerAspectCrop(width: number, height: number, aspect: number) {
+  return centerCrop(
+    makeAspectCrop(
+      {
+        unit: "px",
+        width: width,
+      },
+      aspect,
+      width,
+      height,
+    ),
+    width,
+    height,
+  );
+}
+
+export default function ImageCrop({
+  src,
+  updateData,
+}: {
+  src: string;
+  updateData: (data: string) => void;
+}) {
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  const [crop, setCrop] = useState<Crop>();
+
+  function onImageLoad(e: React.SyntheticEvent<HTMLImageElement>) {
+    const width = e.currentTarget.width;
+    const height = e.currentTarget.height;
+
+    setCrop(centerAspectCrop(width, height, 4 / 3));
+  }
+
+  function finishCrop() {
+    if (!imgRef.current || !crop) {
+      return;
+    }
+
+    let c = crop;
+
+    if (c.width === 0 || c.height === 0) {
+      const width = imgRef.current.width;
+      const height = imgRef.current.height;
+      c = centerAspectCrop(width, height, 4 / 3);
+    }
+
+    const canvas = document.createElement("canvas");
+    const scaleX = imgRef.current.naturalWidth / imgRef.current.width;
+    const scaleY = imgRef.current.naturalHeight / imgRef.current.height;
+    const ctx = canvas.getContext("2d");
+
+    if (!ctx) {
+      return;
+    }
+
+    canvas.width = c.width;
+    canvas.height = c.height;
+
+    ctx.drawImage(
+      imgRef.current,
+      (c.x || 0) * scaleX,
+      (c.y || 0) * scaleY,
+      c.width * scaleX,
+      c.height * scaleY,
+      0,
+      0,
+      c.width,
+      c.height,
+    );
+
+    updateData(canvas.toDataURL("image/jpeg"));
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <ReactCrop
+        crop={crop}
+        onChange={(c) => setCrop(c)}
+        aspect={4 / 3}
+        className="max-w-fit shadow-xl"
+      >
+        <img ref={imgRef} src={src} onLoad={onImageLoad} />
+      </ReactCrop>
+      <Button onClick={finishCrop}>Save</Button>
+    </div>
+  );
+}
